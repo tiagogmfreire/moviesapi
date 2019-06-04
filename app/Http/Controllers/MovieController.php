421f56086d62c768bebfef69dee2382699e45e35
Tiago Galvão M. Freire <tiagogmfreire@gmail.com>
2019-06-04 15:34:16 -0300

    Adding MovieService.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\MovieService;
 
+/**
+ * Controller for the movie related API endpoints
+ */
 class MovieController extends Controller
 {
-    public function movie($id, Request $request)
+    /**
+     * Action for the API endpoint that shows the details
+     * of a particular movie by it's TMDb id.
+     *
+     * @param mixed $id
+     * @param Request $request
+     * @param MovieService $movieService
+     * @return void
+     */
+    public function movie($id, Request $request, MovieService $movieService)
     {
         
         return $id;
---
+++ app/Services/MovieService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Service class to abstract logic regarding retrieving movie information
+ * from the TMDb API.
+ */
+class MovieService
+{
+    public function upcoming()
+    {
+
+    }
+
+    public function search(string $title)
+    {
+
+    }
+
+    public function getDetails(integer $id)
+    {
+
+    }
+}
